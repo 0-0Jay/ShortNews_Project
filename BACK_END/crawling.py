772f8906045b4f028566ccc91c0b80c6fb0a6b89
junsgi <qwer3932@gmail.com>
2023-10-17 12:41:56 +0900

    1017

--- BACK_END/crawling.py
+++ BACK_END/crawling.py
@@ -67,7 +67,38 @@ for i in range(1, 2):
 
         #title = temp[space + 1:].strip()
         
-        wr.writerow([title, link])
+        wr.writerow([title, text, link])
+
+
+    # 본문 가져오는 코드
+    ul_list = parsed_source.find_all("ul", class_="type06")
+    
+    ul_list = ul_list[0]
+    
+    # ul -> dt태그 의 두 번째 자식 태그
+    div_image_list = ul_list.select("dt:nth-child(2)")
+    for idx, item in enumerate(div_image_list):
+        temp = basic_clear(str(item)) # 특수기호 및 공백 처리
+
+        space = temp.index(' ')
+        link = temp[:space]
+
+        article = Article(link, language = "ko")
+        article.download()
+        article.parse()
+        title = article.title
+        text = article.text
+
+        #title = temp[space + 1:].strip()
+        
+        wr.writerow([title, text, link])
+
+
+f1.close()
+f1 = open(r'.\test.csv', 'r', encoding='utf-8')
+spamreader = csv.reader(f1)
+for row in spamreader:
+    print(row)
 f1.close()
 """
 ###pandas code###
