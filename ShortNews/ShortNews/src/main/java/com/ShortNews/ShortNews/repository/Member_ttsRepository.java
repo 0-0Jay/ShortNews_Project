ce344002933db0fe4d142a0b64b43909bd93f635
조영진 <117143398+0-0Jay@users.noreply.github.com>
2024-01-12 15:51:40 +0900

    Delete ShortNews directory

--- ShortNews/ShortNews/src/main/java/com/ShortNews/ShortNews/repository/Member_ttsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ShortNews.ShortNews.repository;
-
-import com.ShortNews.ShortNews.entity.Member_tts;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Member_ttsRepository extends JpaRepository<Member_tts, String> {
-}
