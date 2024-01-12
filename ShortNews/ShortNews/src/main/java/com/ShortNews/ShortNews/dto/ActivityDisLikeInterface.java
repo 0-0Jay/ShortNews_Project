84e08de3870189385906cb6ff2474c5d9ecdbc04
jonghocode <whdgh256@naver.com>
2024-01-12 15:18:10 +0900

    백엔드

---
+++ ShortNews/ShortNews/src/main/java/com/ShortNews/ShortNews/dto/ActivityDisLikeInterface.java
@@ -0,0 +1,9 @@
+package com.ShortNews.ShortNews.dto;
+
+public interface ActivityDisLikeInterface {
+
+    String getId();
+    String getContent();
+    Integer getType();
+    String getTime();
+}
